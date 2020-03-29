fa131c9b633656a325283023b658420408ccd265
Momin-C <hellomomins@yahoo.com>
2020-03-29 00:44:46 -0400

    will work now

--- 2019/J3.py
+++ 2019/J3.py
@@ -1,11 +1,16 @@
 L = int(input())
 lst = []
-
+x = 1
 for counter in range (L):
     lst1 = []
     text = input()
     while text != "":
         num = text.count(text[0])
+        if num > 0:
+            for counter in range (1,num):
+                if text[counter] != text[counter-1]:
+                    num = counter
+                    break
         lst1.append(str(num) + " " + text[0])
         text = text[num:]
     lst.append(" ".join(lst1))
