601850a20fb4af8fa02c1015e5c32a69dc079455
Momin-C <hellomomins@yahoo.com>
2020-03-29 14:39:14 -0400

    Improved efficiency

--- 2019/J3.py
+++ 2019/J3.py
@@ -5,7 +5,7 @@ for counter in range (L):
     text = input()
     while text != "":
         num = text.count(text[0])
-        if num > 0:
+        if num > 1:
             for counter in range (1,num):
                 if text[counter] != text[counter-1]:
                     num = counter
@@ -13,6 +13,5 @@ for counter in range (L):
         lst1.append(str(num) + " " + text[0])
         text = text[num:]
     lst.append(" ".join(lst1))
-
 for counter in range (len(lst)):
     print (lst[counter])
---
+++ 2019/J4.py
@@ -0,0 +1,9 @@
+grid = ["1 2","3 4"]
+seq = input()
+for i in range (len(seq)):
+    if seq[i] == "V":
+        grid = grid[::-1]
+    elif seq[i] == "H":
+        grid[0],grid[1] = grid[1],grid[0]
+for i in range (2):
+    print (grid[counter])
