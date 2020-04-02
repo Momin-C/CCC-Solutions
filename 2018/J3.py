bfec74cf6cf18b5255b01490f92938194fdfab94
Momin-C <hellomomins@yahoo.com>
2020-04-02 14:56:08 -0400

    Finished J3

--- 2018/J3.py
+++ 2018/J3.py
@@ -1,17 +1,29 @@
-lst = []
-distance1 = []
-distance2 = []
-diff = 0
-distances = input().split()
-for i in range (len(distances)):
-    distances[i] = int(distances[i])
+d1,d2,d3,d4,d5 = [],[],[],[],[]
+
+d = input().split()
+for i in range (len(d)):
+    d[i] = int(d[i])
 
 for i in range (5):
-    diff = sum(distances[:i])
-    distance1.append(diff)
-print (distance1)
-distance2.append(distances[0])
-for i in range(5):
-    diff = sum(distances[:i])
-    distance2.append(diff)
-print (distance2)
+    d1.append(sum(d[:i]))
+    d2.append(sum(d[1:i]))
+    d3.append(sum(d[2:i]))
+    d4.append(sum(d[3:i]))
+    d5.append(sum(d[4:i]))
+
+d2[0] = d[0]
+
+d3[1] = d[1]
+d3[0] = d[1] + d[0]
+
+d4[2] = d[2]
+d4[1] = d[2] + d[1]
+d4[0] = d[2] + d[1] + d[0]
+
+d5[3] = d[3]
+d5[2] = d[3] + d[2]
+d5[1] = d[3] + d[2] + d[1]
+d5[0] = d[3] + d[2] + d[1] + d[0]
+
+for a,b,c,d,e in d1,d2,d3,d4,d5:
+    print (a,b,c,d,e)
