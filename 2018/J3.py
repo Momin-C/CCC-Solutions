5e9968f2a1e937464b113cfe9639416162af0287
Momin-C <hellomomins@yahoo.com>
2020-04-02 16:06:16 -0400

    Improved efficiency

--- 2018/J3.py
+++ 2018/J3.py
@@ -1,9 +1,9 @@
 d1,d2,d3,d4,d5 = [],[],[],[],[]
-
+"""
 d = input().split()
 for i in range (len(d)):
-    d[i] = int(d[i])
-
+    d[i] = int(d[i])"""
+d = [3,10,12,5]
 for i in range (5):
     d1.append(sum(d[:i]))
     d2.append(sum(d[1:i]))
@@ -13,17 +13,17 @@ for i in range (5):
 
 d2[0] = d[0]
 
-d3[1] = d[1]
-d3[0] = d[1] + d[0]
+d3[1] = sum(d[1:2])
+d3[0] = sum(d[:2])
 
-d4[2] = d[2]
-d4[1] = d[2] + d[1]
-d4[0] = d[2] + d[1] + d[0]
+d4[2] = sum(d[2:3])
+d4[1] = sum(d[1:3])
+d4[0] = sum(d[:3])
 
-d5[3] = d[3]
-d5[2] = d[3] + d[2]
-d5[1] = d[3] + d[2] + d[1]
-d5[0] = d[3] + d[2] + d[1] + d[0]
+d5[3] = sum(d[3:4])
+d5[2] = sum(d[2:4])
+d5[1] = sum(d[1:4])
+d5[0] = sum(d[:4])
 
 for a,b,c,d,e in d1,d2,d3,d4,d5:
     print (a,b,c,d,e)
---
+++ 2018/J3V2.py
@@ -0,0 +1,6 @@
+d1,d2,d3,d4,d5 = [],[],[],[],[]
+d = [3,10,12,5]
+for i in range (1,3):
+    list(d*i).append("Memes")
+
+print (d1)
