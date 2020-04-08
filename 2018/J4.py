56cc36d04cee8a032e3e2ec85fa43d8136a3c610
Momin-C <hellomomins@yahoo.com>
2020-04-07 23:17:44 -0400

    Started work on J4

--- 2018/J4.py
+++ 2018/J4.py
@@ -1,5 +1,9 @@
 L = int(input())
-a = [[]]
-for counter in range (L):
+a = []
+for i in range (L):
     digits = input().split()
+    a.append(digits)
 print (a)
+for i in range (2):
+    print (a[i])
+print (min(a))
