ef3e947d6176218240edecfae76f8b5808dfa0f2
Momin-C <hellomomins@yahoo.com>
2020-04-10 21:25:19 -0400

    Reorganized into language folders (due to Java implementation)

---
+++ 2020/Java/J1.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class J1 {
+	public static void main(String args[]) {
+        Scanner input = new Scanner(System.in);
+        int S = input.nextInt();
+        int M = input.nextInt();
+        int L = input.nextInt();
+        if (S+M*2+L*3 >= 10){
+            System.out.println("happy");
+        }
+        else {
+            System.out.println("sad");
+        }
+    }
+}
---
+++ 2020/Java/J2.java
@@ -0,0 +1,7 @@
+import java.util.*;
+public class J2 {
+	public static void main(String args[]) {
+        Scanner input = new Scanner(System.in);
+        int P = input.nextInt();
+        int N = input.nextInt();
+        int R = input.nextInt();
