e917cf2d98f630b942bf507491f3272ea91dd683
Momin-C <hellomomins@yahoo.com>
2020-04-10 22:52:05 -0400

    Implementing solutions in Java, reorganized into different folders

---
+++ 2019/Java/J1.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class J1 {
+	public static void main(String args[]) {
+        Scanner input = new Scanner(System.in);
+        int A3 = input.nextInt();
+        int A2 = input.nextInt();
+        int A1 = input.nextInt();
+
+        int B3 = input.nextInt();
+        int B2 = input.nextInt();
+        int B1 = input.nextInt();
+        if (A3*3 + A2*2 + A1 > B3*3 + B2*2 + B1){
+            System.out.println("A");
+        }
+        else if (A3*3 + A2*2 + A1 == B3*3 + B2*2 + B1) {
+            System.out.println("T");
+        }
+        else{
+            System.out.println("B");
+        }
+    }
+}
