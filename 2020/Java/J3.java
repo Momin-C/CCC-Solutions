3db6c388033e42e10c305a53179d1c35fe5b3524
Momin-C <hellomomins@yahoo.com>
2020-04-22 16:06:45 -0400

    Resolving issues with 2020 J3 Java

--- 2020/Java/J3.java
+++ 2020/Java/J3.java
@@ -4,18 +4,19 @@ public class J3 {
         Scanner input = new Scanner (System.in);
         ArrayList<String> dropsX = new ArrayList<String>();
         ArrayList<String> dropsY = new ArrayList<String>();
-
+		String[] splitStr;
         int L = input.nextInt();
         String str;
-        for (int i=0; i<L;i++){
-            str = input.next();
-            String[] splitStr = str.trim().split("\\s+ ");
 
-
+        for (int i=0; i<=L;i++){
+            str = input.nextLine();
+			splitStr = str.split(", ");
+			System.out.println(splitStr[0]);
+			System.out.println(splitStr[1]);
+			/*
             dropsX.add(splitStr[0]);
-            System.out.println(dropsX);
-            dropsY.add(splitStr[1]);
-            System.out.println(dropsY);
+			dropsY.add(splitStr[1]);
+			*/
         }
 
     }
