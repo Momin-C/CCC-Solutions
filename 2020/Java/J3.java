196e04f7a984fd06bfa5c8b81f6daadbd7473d47
Momin-C <hellomomins@yahoo.com>
2020-04-22 17:08:50 -0400

    Finished J3 in Java

--- 2020/Java/J3.java
+++ 2020/Java/J3.java
@@ -2,22 +2,20 @@ import java.util.*;
 public class J3 {
 	public static void main(String args[]) {
         Scanner input = new Scanner (System.in);
-        ArrayList<String> dropsX = new ArrayList<String>();
-        ArrayList<String> dropsY = new ArrayList<String>();
-		String[] splitStr;
+        List<String> dropsX = new ArrayList<String>();
+        List<String> dropsY = new ArrayList<String>();
+		List<Integer> dropsXInt = new ArrayList<Integer>();
+		List<Integer> dropsYInt = new ArrayList<Integer>();
         int L = input.nextInt();
-        String str;
-
-        for (int i=0; i<=L;i++){
-            str = input.nextLine();
-			splitStr = str.split(", ");
-			System.out.println(splitStr[0]);
-			System.out.println(splitStr[1]);
-			/*
-            dropsX.add(splitStr[0]);
+        for (int i=0; i<L;i++){
+            String str = input.next();
+			String[] splitStr = str.split(",");
+			dropsX.add(splitStr[0]);
 			dropsY.add(splitStr[1]);
-			*/
         }
-
+		for (String s : dropsX) dropsXInt.add(Integer.valueOf(s));
+		for (String s : dropsY) dropsYInt.add(Integer.valueOf(s));
+		System.out.println((Collections.min(dropsXInt) -1) + "," + (Collections.min(dropsYInt) -1));
+		System.out.println((Collections.max(dropsXInt) +1) + "," + (Collections.max(dropsYInt) +1));
     }
 }
