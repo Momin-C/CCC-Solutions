fa081f46db3f2b473367824a4b8702ef28e7e1c8
Momin-C <hellomomins@yahoo.com>
2020-04-22 17:25:15 -0400

    Finished J4

---
+++ 2020/Java/J4.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class J4 {
+	public static void main(String args[]) {
+        Scanner input = new Scanner (System.in);
+        String T = input.next();
+        String S = input.next();
+	    for (int i = 0; i<S.length();i++){
+            S = S.substring(1) + S.charAt(0);
+            if (T.contains(S)){
+                System.out.println("yes");
+                break;
+            }
+        }
+        if (T.contains(S) == false){
+            System.out.println("no");
+        }
+    }
+}
