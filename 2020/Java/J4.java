c4d49b8c5cca9c61daa0e18d322cb64e40af6ac3
Momin Chaudhry <62663634+Momin-C@users.noreply.github.com>
2020-04-22 17:32:11 -0400

    Enhanced readability for git

--- 2020/Java/J4.java
+++ 2020/Java/J4.java
@@ -4,12 +4,12 @@ public class J4 {
         Scanner input = new Scanner (System.in);
         String T = input.next();
         String S = input.next();
-	    for (int i = 0; i<S.length();i++){
-            S = S.substring(1) + S.charAt(0);
-            if (T.contains(S)){
-                System.out.println("yes");
-                break;
-            }
+	for (int i = 0; i<S.length();i++){
+        	S = S.substring(1) + S.charAt(0);
+        	if (T.contains(S)){
+                	System.out.println("yes");
+                	break;
+            	}
         }
         if (T.contains(S) == false){
             System.out.println("no");
