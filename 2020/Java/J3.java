21a7abbc8fbd9617947e4d0b2739fc54f24ef7fe
Momin Chaudhry <62663634+Momin-C@users.noreply.github.com>
2020-04-22 17:30:21 -0400

    Fixed indenting to enhance readability

--- 2020/Java/J3.java
+++ 2020/Java/J3.java
@@ -4,18 +4,18 @@ public class J3 {
         Scanner input = new Scanner (System.in);
         List<String> dropsX = new ArrayList<String>();
         List<String> dropsY = new ArrayList<String>();
-		List<Integer> dropsXInt = new ArrayList<Integer>();
-		List<Integer> dropsYInt = new ArrayList<Integer>();
+	List<Integer> dropsXInt = new ArrayList<Integer>();
+	List<Integer> dropsYInt = new ArrayList<Integer>();
         int L = input.nextInt();
         for (int i=0; i<L;i++){
-            String str = input.next();
-			String[] splitStr = str.split(",");
-			dropsX.add(splitStr[0]);
-			dropsY.add(splitStr[1]);
+            	String str = input.next();
+		String[] splitStr = str.split(",");
+		dropsX.add(splitStr[0]);
+		dropsY.add(splitStr[1]);
         }
-		for (String s : dropsX) dropsXInt.add(Integer.valueOf(s));
-		for (String s : dropsY) dropsYInt.add(Integer.valueOf(s));
-		System.out.println((Collections.min(dropsXInt) -1) + "," + (Collections.min(dropsYInt) -1));
-		System.out.println((Collections.max(dropsXInt) +1) + "," + (Collections.max(dropsYInt) +1));
+	for (String s : dropsX) dropsXInt.add(Integer.valueOf(s));
+	for (String s : dropsY) dropsYInt.add(Integer.valueOf(s));
+	System.out.println((Collections.min(dropsXInt) -1) + "," + (Collections.min(dropsYInt) -1));
+	System.out.println((Collections.max(dropsXInt) +1) + "," + (Collections.max(dropsYInt) +1));
     }
 }
