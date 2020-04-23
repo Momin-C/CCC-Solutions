17f19f72c8f18e5d070780ba10aca64b575ff708
Momin-C <hellomomins@yahoo.com>
2020-04-23 14:25:34 -0400

    Finished J2

--- 2019/Java/J2.java
+++ 2019/Java/J2.java
@@ -2,22 +2,23 @@ import java.util.*;
 public class J2 {
 	public static void main(String args[]) {
         Scanner input = new Scanner(System.in);
-        List<String> num = new ArrayList<String>();
-	    List<Integer> numInt = new ArrayList<Integer>();
+        List<Integer> num = new ArrayList<Integer>();
 	    List<String> character = new ArrayList<String>();
-        //String[] splitStr;
+        List<String> printable = new ArrayList<String>();
         int L = input.nextInt();
-        System.out.println("Integer L is " + L);
         for (int i = 0; i<L;i++){
-            numInt
-            input.nextLine();
-            String str = input.nextLine();
-            System.out.println("Str is: " + str);
-            String[] splitStr = str.split("\\s");
-            System.out.println("The length of the split string is: " + splitStr.length);
-            //System.out.println(Arrays.toString(splitStr));
-            System.out.println("[ " + splitStr[0] + " , " +splitStr[1] + " ]");
+            String toAdd = "";
+            int number = input.nextInt();
+            String c = input.next();
+            num.add(number);
+            character.add(c);
+            for (int q = 0; q<number;q++){
+                toAdd+=c;
+            }
+            printable.add(toAdd);
         }
-        //for (String s : num) numInt.add(Integer.valueOf(s));
+        for (int x = 0; x<L; x++){
+            System.out.println(printable.get(x));
+        }
     }
 }
