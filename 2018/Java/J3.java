9c9277fb05a4c240c8a37f3124f8a0c06797a6fb
Momin-C <hellomomins@yahoo.com>
2020-04-25 16:43:08 -0400

    Finished J3

---
+++ 2018/Java/J3.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class J3 {
+	public static void main(String args[]) {
+        Scanner input = new Scanner(System.in);
+        int d1 = 3; //input.nextInt();
+        int d2 = 10; //input.nextInt();
+        int d3 = 12; //input.nextInt();
+        int d4 = 5; //input.nextInt();
+
+        System.out.println("0 " + d1 + " " + (d1+d2) + " " + (d1+d2+d3) + " "+  (d1+d2+d3+d4));
+        System.out.println(d1 + " 0 " + d2 + " " + (d2+d3) + " " +  (d2+d3+d4));
+        System.out.println((d1+d2) + " " + d2 + " 0 " + (d3) + " " +  (d3+d4));
+        System.out.println((d1+d2+d3) + " " + (d2+d3) + " " + d3 + " 0 " + d4);
+        System.out.println((d1+d2+d3+d4) + " " + (d2+d3+d4) + " " + (d3+d4) + " " + d4 + " 0");
+
+    }
+}
