cb0a0bb6719f3532c07a0685b16642accff2b13a
Momin-C <hellomomins@yahoo.com>
2020-04-26 15:22:49 -0400

    Finished J2

---
+++ 2017/Java/J2.java
@@ -0,0 +1,19 @@
+/*
+lst = [] #SOLUTION 1
+for i in range (K+1):
+    lst.append(N*10**i)
+print (sum(lst))*/
+
+import java.util.*;
+public class J2 {
+	public static void main(String args[]) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        int k = input.nextInt();
+        double sum = 0;
+        for (int i = 0; i<k+1; i++){
+            sum+=n*Math.pow(10,i);
+        }
+        System.out.println((int)sum);
+    }
+}
