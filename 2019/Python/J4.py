3f1e308bc8b17d6a695efc11dd92ad0fdf11f8c2
Momin-C <hellomomins@yahoo.com>
2020-05-01 19:16:21 -0400

    Corrected J4

--- 2019/Python/J4.py
+++ 2019/Python/J4.py
@@ -2,8 +2,8 @@ grid = ["1 2","3 4"]
 seq = input()
 for i in range (len(seq)):
     if seq[i] == "V":
-        grid = grid[::-1]
+        grid[0],grid[1] = grid[0][::-1],grid[1][::-1]
     elif seq[i] == "H":
         grid[0],grid[1] = grid[1],grid[0]
 for i in range (2):
-    print (grid[counter])
+    print (grid[i])
