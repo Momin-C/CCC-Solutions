30d7a21819d95fa103670b061265279717da0f51
Momin-C <hellomomins@yahoo.com>
2020-05-22 20:25:02 -0400

    Finished J3

---
+++ 2016/Python/J3.py
@@ -0,0 +1,7 @@
+lst = []
+word = input()
+for i in range(len(word)):
+    for x in range(len(word)-1,-1,-1):
+        if word[i:x+1]!= "" and word[i:x+1] == word[i:x+1][::-1]:
+            lst.append(len(word[i:x+1]))
+print(max(lst))
