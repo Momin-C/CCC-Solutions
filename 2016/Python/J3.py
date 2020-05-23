5c89f78b1851b296d4b20ed3d9e07635adeab4fd
Momin-C <hellomomins@yahoo.com>
2020-05-23 19:42:04 -0400

    Optimized J3

--- 2016/Python/J3.py
+++ 2016/Python/J3.py
@@ -4,4 +4,5 @@ for i in range(len(word)):
     for x in range(len(word)-1,-1,-1):
         if word[i:x+1]!= "" and word[i:x+1] == word[i:x+1][::-1]:
             lst.append(len(word[i:x+1]))
+            break
 print(max(lst))
