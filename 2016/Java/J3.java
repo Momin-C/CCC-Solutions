0050cb75f2d2cf98d2402169c4d66ad99413240b
Momin-C <hellomomins@yahoo.com>
2020-05-26 22:31:26 -0400

    Started work on J3

---
+++ 2016/Java/J3.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Main {
+	public static void main(String args[]) {
+        Scanner input = new Scanner(System.in);
+        String word = input.nextInt();
+        List<Integer> lst = new ArrayList<Integer>();
+        for (int i = 0; i<word.length; i++){
+            for (int x = word.length; x>-1; x--){
+                if (word.substring(i,x+1) != "" && word.substring(i,x+1) == new StringBuilder(word.substring(i,x+1)).reverse().toString()){
+                    lst.add(Math.abs(x+1-i))
+                    break;
+                }
+            }
+        }
+        System.out.println((Collections.max(lst)
+    }
+}
