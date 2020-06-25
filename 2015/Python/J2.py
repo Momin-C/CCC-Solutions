acbc22b408762a9e34d06a4e86c8ca6e6ae9c52f
Momin-C <hellomomins@yahoo.com>
2020-06-24 20:59:01 -0400

    Finished J2

---
+++ 2015/Python/J2.py
@@ -0,0 +1,11 @@
+s = input()
+happy = s.count(":-)")
+sad = s.count(":-(")
+if happy == 0 and sad == 0:
+    print ("none")
+elif happy == sad:
+    print ("unsure")
+elif happy > sad:
+    print ("happy")
+else:
+    print ("sad")
